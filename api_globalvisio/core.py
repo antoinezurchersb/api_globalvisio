abb68b8215c328cc56d2dcd0ff761a8496752a52
Antoine Zürcher <zurcher@solares-bauen.fr>
2024-04-23 15:16:24 +0200

    init 6

--- api_globalvisio/core.py
+++ api_globalvisio/core.py
@@ -45,165 +45,6 @@ def get_token():
         return None
 
 
-def get_history(point_id, start, end):
-    """
-    Récupère l'historique horaire en kWh d'un point via des requêtes GET.
-    Gère les périodes de plus de 3 mois en divisant la requête en plusieurs sous-requêtes.
-    Dates au format 'yyyy-mm-dd'.
-    """
-    get_token()
-    if token_info['token'] is None:
-        return None
-
-    # Convertir les chaînes de dates en objets datetime
-    start_date = datetime.strptime(start, '%Y-%m-%d')
-    end_date = datetime.strptime(end, '%Y-%m-%d')
-    max_diff = timedelta(days=88)  # 3 mois maximum
-    data_frames = []  # Pour stocker les résultats de chaque sous-requête
-
-    while start_date < end_date:
-        # Calculer la fin de la période de sous-requête
-        sub_end_date = min(start_date + max_diff, end_date)
-        # Formater les dates pour l'URL
-        sub_start = start_date.strftime('%Y-%m-%d')
-        sub_end = sub_end_date.strftime('%Y-%m-%d')
-
-        url = f'https://global-visio.com/api/points/history/{point_id}?dateStart={sub_start}&dateEnd={sub_end}'
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {token_info["token"]}'
-        }
-
-        try:
-            response = requests.get(url, headers=headers)
-            if response.status_code != 200:
-                print(f"ERREUR lors de la requête d'historique avec l'API de GlobalVisio: {response.json()['message']}")
-                return None
-            response.raise_for_status()
-
-            # Traitement et stockage des données reçues
-            if response.json()['response']['history']:
-                sub_data = pd.DataFrame(response.json()['response']['history'])
-                sub_data = sub_data[['date', 'value']]
-                sub_data['date'] = pd.to_datetime(sub_data['date'], utc=True)
-                sub_data = sub_data[sub_data['date'].dt.second == 0]
-                sub_data = sub_data.sort_values(by=['date', 'value'], ascending=[True, True])
-                sub_data['date'] = sub_data['date'].dt.tz_convert('Europe/Paris')
-                sub_data.drop_duplicates(subset='date', keep='first', inplace=True)
-                sub_data.set_index('date', inplace=True)
-                sub_data['unit'] = response.json()['response']['point']['unit']['symbole']
-                data_frames.append(sub_data)
-            else:
-                print(
-                    f"ERREUR lors de la requête d'historique avec l'API de GlobalVisio: données inexistantes pour le point {point_id} entre {sub_start} et {sub_end}")
-
-        except requests.RequestException as e:
-            print(f"ERREUR lors de la requête d'historique avec l'API de GlobalVisio: {e}")
-            return None
-        except json.JSONDecodeError:
-            print('ERREUR de décodage JSON. Vérifiez le format de la réponse.')
-            return None
-        except KeyError:
-            print('ERREUR dans la structure de données reçue. Vérifiez le format des données.')
-            return None
-
-        # Préparer la date de début pour la prochaine sous-requête
-        start_date = sub_end_date + timedelta(days=1)
-
-    # Fusionner les résultats de toutes les sous-requêtes
-    if data_frames:
-        df_concat = pd.concat(data_frames)
-        df_concat = df_concat[df_concat['value'] != 0.0]
-        # Si les valeurs sont un index
-        if df_concat['value'].is_monotonic_increasing:
-            df_concat = df_concat[(df_concat.index.minute == 0) & (df_concat.index.second == 0)]
-            df_concat['value'] = df_concat.iloc[:, 0].diff()
-            df_concat.iloc[0, 0] = 0.0
-        # Si les valeurs sont des consommations horaires ou moins
-        else:
-            unit = df_concat.iloc[0, 1]
-            df_concat = df_concat['value'].resample('h').mean().to_frame()
-            df_concat['unit'] = unit
-
-        return df_concat
-    else:
-        return None
-
-
-def get_consumption_day(point_id, start, end):
-    """
-    Récupère l'historique journalier en kWh d'un point via des requêtes GET.
-    Gère les périodes de plus de 1 an en divisant la requête en plusieurs sous-requêtes.
-    Dates au format 'yyyy-mm-dd'.
-    """
-    get_token()
-    if token_info['token'] is None:
-        return None
-
-    # Convertir les chaînes de dates en objets datetime
-    start_date = datetime.strptime(start, '%Y-%m-%d')
-    end_date = datetime.strptime(end, '%Y-%m-%d')
-    max_diff = timedelta(days=364)  # 1 an maximum
-    data_frames = []  # Pour stocker les résultats de chaque sous-requête
-
-    while start_date < end_date:
-        # Calculer la fin de la période de sous-requête
-        sub_end_date = min(start_date + max_diff, end_date)
-        # Formater les dates pour l'URL
-        sub_start = start_date.strftime('%Y-%m-%d')
-        sub_end = sub_end_date.strftime('%Y-%m-%d')
-
-        url = f'https://global-visio.com/api/points/consumption/{point_id}?dateStart={sub_start}&dateEnd={sub_end}&period=2'
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {token_info["token"]}'
-        }
-
-        try:
-            response = requests.get(url, headers=headers)
-            if response.status_code != 200:
-                print(
-                    f"ERREUR lors de la requête de consommation journalière avec l'API de GlobalVisio: {response.json()['message']}")
-                return None
-            response.raise_for_status()
-
-            # Traitement et stockage des données reçues
-            if response.json()['response']['history']:
-                sub_data = pd.DataFrame(response.json()['response']['history'])
-                sub_data = sub_data[['date', 'value']]
-                sub_data['unit'] = response.json()['response']['point']['unit']['symbole']
-                sub_data['date'] = pd.to_datetime(sub_data['date'], utc=True)
-                sub_data['date'] = sub_data['date'].dt.tz_convert('Europe/Paris')
-                sub_data.drop_duplicates(subset='date', keep='first', inplace=True)
-                sub_data.set_index('date', inplace=True)
-                data_frames.append(sub_data)
-            else:
-                print(
-                    f"ERREUR lors de la requête de consommation journalière avec l'API de GlobalVisio: données inexistantes pour le point {point_id} entre {sub_start} et {sub_end}")
-
-        except requests.RequestException as e:
-            print(f"ERREUR lors de la requête de consommation journalière avec l'API de GlobalVisio: {e}")
-            return None
-        except json.JSONDecodeError:
-            print('ERREUR de décodage JSON. Vérifiez le format de la réponse.')
-            return None
-        except KeyError:
-            print('ERREUR dans la structure de données reçue. Vérifiez le format des données.')
-            return None
-
-        # Préparer la date de début pour la prochaine sous-requête
-        start_date = sub_end_date + timedelta(days=1)
-
-    # Fusionner les résultats de toutes les sous-requêtes
-    if data_frames:
-        df_concat = pd.concat(data_frames)
-        df_concat['value'] = df_concat.iloc[:, 0].diff()
-        df_concat.iloc[0, 0] = 0.0
-        return df_concat
-    else:
-        return None
-
-
 def get_site_id_from_char(char):
     """
     Récupère le site dont le nom contient les caractères spécifiés via une requête GET.
@@ -468,6 +309,162 @@ class Point:
             print('ERREUR dans la structure de données reçue. Vérifiez le format des données.')
             return None
 
+    def get_history(self, start, end):
+        """
+        Récupère l'historique horaire en kWh d'un point via des requêtes GET.
+        Gère les périodes de plus de 3 mois en divisant la requête en plusieurs sous-requêtes.
+        Dates au format 'yyyy-mm-dd'.
+        """
+        get_token()
+        if token_info['token'] is None:
+            return None
+
+        # Convertir les chaînes de dates en objets datetime
+        start_date = datetime.strptime(start, '%Y-%m-%d')
+        end_date = datetime.strptime(end, '%Y-%m-%d')
+        max_diff = timedelta(days=88)  # 3 mois maximum
+        data_frames = []  # Pour stocker les résultats de chaque sous-requête
+
+        while start_date < end_date:
+            # Calculer la fin de la période de sous-requête
+            sub_end_date = min(start_date + max_diff, end_date)
+            # Formater les dates pour l'URL
+            sub_start = start_date.strftime('%Y-%m-%d')
+            sub_end = sub_end_date.strftime('%Y-%m-%d')
+
+            url = f'https://global-visio.com/api/points/history/{self.id}?dateStart={sub_start}&dateEnd={sub_end}'
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {token_info["token"]}'
+            }
+
+            try:
+                response = requests.get(url, headers=headers)
+                if response.status_code != 200:
+                    print(
+                        f"ERREUR lors de la requête d'historique avec l'API de GlobalVisio: {response.json()['message']}")
+                    return None
+                response.raise_for_status()
+
+                # Traitement et stockage des données reçues
+                if response.json()['response']['history']:
+                    sub_data = pd.DataFrame(response.json()['response']['history'])
+                    sub_data = sub_data[['date', 'value']]
+                    sub_data['date'] = pd.to_datetime(sub_data['date'], utc=True)
+                    sub_data = sub_data[sub_data['date'].dt.second == 0]
+                    sub_data = sub_data.sort_values(by=['date', 'value'], ascending=[True, True])
+                    sub_data['date'] = sub_data['date'].dt.tz_convert('Europe/Paris')
+                    sub_data.drop_duplicates(subset='date', keep='first', inplace=True)
+                    sub_data.set_index('date', inplace=True)
+                    sub_data['unit'] = response.json()['response']['point']['unit']['symbole']
+                    data_frames.append(sub_data)
+                else:
+                    print(
+                        f"ERREUR lors de la requête d'historique avec l'API de GlobalVisio: données inexistantes pour le point {self.id} entre {sub_start} et {sub_end}")
+
+            except requests.RequestException as e:
+                print(f"ERREUR lors de la requête d'historique avec l'API de GlobalVisio: {e}")
+                return None
+            except json.JSONDecodeError:
+                print('ERREUR de décodage JSON. Vérifiez le format de la réponse.')
+                return None
+            except KeyError:
+                print('ERREUR dans la structure de données reçue. Vérifiez le format des données.')
+                return None
+
+            # Préparer la date de début pour la prochaine sous-requête
+            start_date = sub_end_date + timedelta(days=1)
+
+        # Fusionner les résultats de toutes les sous-requêtes
+        if data_frames:
+            df_concat = pd.concat(data_frames)
+            df_concat = df_concat[df_concat['value'] != 0.0]
+            # Si les valeurs sont un index
+            if df_concat['value'].is_monotonic_increasing:
+                df_concat = df_concat[(df_concat.index.minute == 0) & (df_concat.index.second == 0)]
+                df_concat['value'] = df_concat.iloc[:, 0].diff()
+                df_concat.iloc[0, 0] = 0.0
+            # Si les valeurs sont des consommations horaires ou moins
+            else:
+                unit = df_concat.iloc[0, 1]
+                df_concat = df_concat['value'].resample('h').mean().to_frame()
+                df_concat['unit'] = unit
+
+            return df_concat
+        else:
+            return None
+
+    def get_consumption_day(self, start, end):
+        """
+        Récupère l'historique journalier en kWh d'un point via des requêtes GET.
+        Gère les périodes de plus de 1 an en divisant la requête en plusieurs sous-requêtes.
+        Dates au format 'yyyy-mm-dd'.
+        """
+        get_token()
+        if token_info['token'] is None:
+            return None
+
+        # Convertir les chaînes de dates en objets datetime
+        start_date = datetime.strptime(start, '%Y-%m-%d')
+        end_date = datetime.strptime(end, '%Y-%m-%d')
+        max_diff = timedelta(days=364)  # 1 an maximum
+        data_frames = []  # Pour stocker les résultats de chaque sous-requête
+
+        while start_date < end_date:
+            # Calculer la fin de la période de sous-requête
+            sub_end_date = min(start_date + max_diff, end_date)
+            # Formater les dates pour l'URL
+            sub_start = start_date.strftime('%Y-%m-%d')
+            sub_end = sub_end_date.strftime('%Y-%m-%d')
+
+            url = f'https://global-visio.com/api/points/consumption/{self.id}?dateStart={sub_start}&dateEnd={sub_end}&period=2'
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {token_info["token"]}'
+            }
+
+            try:
+                response = requests.get(url, headers=headers)
+                if response.status_code != 200:
+                    print(
+                        f"ERREUR lors de la requête de consommation journalière avec l'API de GlobalVisio: {response.json()['message']}")
+                    return None
+                response.raise_for_status()
+
+                # Traitement et stockage des données reçues
+                if response.json()['response']['consumption']:
+                    sub_data = pd.DataFrame(response.json()['response']['consumption'])
+                    sub_data = sub_data[['date', 'value']]
+                    sub_data['unit'] = response.json()['response']['point']['unit']['symbole']
+                    sub_data['date'] = pd.to_datetime(sub_data['date'], utc=True)
+                    sub_data['date'] = sub_data['date'].dt.tz_convert('Europe/Paris')
+                    sub_data.drop_duplicates(subset='date', keep='first', inplace=True)
+                    sub_data.set_index('date', inplace=True)
+                    data_frames.append(sub_data)
+                else:
+                    print(
+                        f"ERREUR lors de la requête de consommation journalière avec l'API de GlobalVisio: données inexistantes pour le point {self.id} entre {sub_start} et {sub_end}")
+
+            except requests.RequestException as e:
+                print(f"ERREUR lors de la requête de consommation journalière avec l'API de GlobalVisio: {e}")
+                return None
+            except json.JSONDecodeError:
+                print('ERREUR de décodage JSON. Vérifiez le format de la réponse.')
+                return None
+            except KeyError:
+                print('ERREUR dans la structure de données reçue. Vérifiez le format des données.')
+                return None
+
+            # Préparer la date de début pour la prochaine sous-requête
+            start_date = sub_end_date + timedelta(days=1)
+
+        # Fusionner les résultats de toutes les sous-requêtes
+        if data_frames:
+            df_concat = pd.concat(data_frames)
+            return df_concat
+        else:
+            return None
+
 
 def get_device_id_from_char(site_id, char):
     """
@@ -572,8 +569,8 @@ def get_points_id_from_char(device_id, char):
         return None
 
 
-
-site = Site(2243)
-device = Equipement(1910)
-point = Point(74720)
-print(1)
+# site = Site(2243)
+# device = Equipement(1910)
+# point = Point(76750)
+# data = point.get_history('2024-01-10', '2024-02-10')
+# print(1)
