cca2f16d7976d9b5f056c2af76fb854f764bbbe9
Antoine Zürcher <zurcher@solares-bauen.fr>
2024-04-30 17:10:03 +0200

    init 12

--- api_globalvisio/core.py
+++ api_globalvisio/core.py
@@ -801,12 +801,4 @@ def get_all_points_from_site(site_id):
 
 
 
-# site = Site(2243)
-# device = Equipement(1910)
-# point = Point(201314)
-# data = point.get_history('2024-01-10', '2024-02-10')
-# point_save = Point(247118)
-# point_save.save_history(data)
-# print(1)
 
-# get_all_points_from_site(3659)
